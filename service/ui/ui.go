099521e37a020c4a38aeb8eeff749db5b2cc3be2
Patrick Hener <patrickhener@gmx.de>
2021-02-17 16:17:52 +0100

    Shutting down with os/exec

--- service/ui/ui.go
+++ service/ui/ui.go
@@ -8,11 +8,10 @@ import (
 	"io/fs"
 	"io/ioutil"
 	"net/http"
+	"os/exec"
 	"strconv"
 	"strings"
 	"time"
-
-	"golang.org/x/sys/unix"
 
 	"github.com/dascr/dascr-machine/service/config"
 	"github.com/dascr/dascr-machine/service/connector"
@@ -254,10 +253,12 @@ func (ws *WebServer) updateScoreboard(w http.ResponseWriter, r *http.Request) {
 func (ws *WebServer) shutdown(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		logger.Debug("Shutdown signal received. Shutting down.")
-		// syscall.Sync()
-		// syscall.Reboot(int(syscall.LINUX_REBOOT_CMD_HALT))
-		h := uint(unix.LINUX_REBOOT_CMD_HALT)
-		unix.Reboot(int(h))
+		cmd := exec.Command("shutdown", "-h", "now")
+		stoutStderr, err := cmd.CombinedOutput()
+		if err != nil {
+			logger.Errorf("Error shutting down: %+v", err)
+		}
+		logger.Debug(string(stoutStderr))
 	}
 }
 
